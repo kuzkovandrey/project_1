e73411dc7a3c410249256ce473c490d59654c294
Andrey Kuzkov <kuzkovandrey@gmail.com>
2020-10-13 20:21:53 +0300

    Split the common CSS stylesheet into section

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@
 const {src, dest, parallel, series, watch} = require('gulp');
 const browserSync = require('browser-sync').create();
 const sass = require('gulp-sass');
+const rename = require('gulp-rename');
 
 function browsersync() {
 	browserSync.init({
@@ -12,8 +13,9 @@ function browsersync() {
 }
 
 function styles() {
-	return src('app/scss/main.scss')
+	return src('app/scss/common.scss')
 	.pipe(sass())
+	.pipe(rename("main.css"))
 	.pipe(dest('app/css/'))
 	.pipe(browserSync.stream());
 }
